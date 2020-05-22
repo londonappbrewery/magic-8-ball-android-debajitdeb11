da1c9e281f7b79d07b920ff50c22f9595f4c2cbf
dasushan <53913896+dasushan@users.noreply.github.com>
2020-05-22 20:25:24 +0530

    Demo.java
    /* git demo1
          method call java */

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    static void myMethod(){
+        System.out.println("Hello , GITHUB");
+        }
+        
+ public static void main (String[] args) {
+       myMethod();
+     }
+    } 
+        
